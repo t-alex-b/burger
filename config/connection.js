3e2ef56168ccacb59f48ec96d6d15446745afeea
t-alex-b <sunnoperatic@aol.com>
2022-01-17 13:05:37 -0700

    using mysql2

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
-var mysql = require("mysql");
+var mysql = require("mysql2");
 
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "password",
+  password: "sneed2thesql",
   database: "burgers_db"
 });
 
